d9fed45e9564ae58bea1b7e389d3399061d102b3
phyouthcenter1 <67077196+phyouthcenter1@users.noreply.github.com>
2022-02-28 06:21:39 -0800

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -7,7 +7,7 @@ function ToWord(value, context)
 	context = { scale = context.scale or 'long'; decimal = context.decimal or 0; };
 	local numbers =
 		{
-			ones = { "ONE", "TWO", "THREE", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
+			ones = { "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
 			tens = { '', "Twenty", "Thirty", "Fourty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
 			thousands =
 			{
